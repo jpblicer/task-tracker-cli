2c48d89029da2a15fd604ce7867659cfecdb7846
James Young <jpblicer@gmail.com>
2024-11-10 17:05:19 +0900

    include json library to write a file with test data

--- main.rb
+++ main.rb
@@ -1,7 +1,22 @@
 require 'optparse'
+require 'json'
 
 parser = OptionParser.new
 
+
+file = "./test_data.json"
+
+data = {
+  "name": "test",
+  "points": 5,
+  "local": "earth"
+}
+
+serialized_data = JSON.pretty_generate(data)
+
+File.open(file, "w") do |file|
+  file.write(serialized_data)
+end
 
 def hello
   "Hello World"
@@ -11,6 +26,7 @@ end
 parser.on('-h', 'Run Hello') do |value|
   puts hello
 end
+
 
 
 parser.parse!
