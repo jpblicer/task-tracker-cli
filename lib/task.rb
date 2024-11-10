95c55756476d567ae2a99abefd39f19bfc50ea40
James Young <jpblicer@gmail.com>
2024-11-10 18:22:37 +0900

    fix attr_accessor syntax, init with only description, add method

--- lib/task.rb
+++ lib/task.rb
@@ -1,13 +1,13 @@
 class Task
 
   attr_reader :id, :created_at
-  attr_accessor :description :status :updated_at
+  attr_accessor :description, :status, :updated_at
   
   @@last_id = 0
 
   STATUS_OPTIONS = ["TODO", "IN-PROGRESS", "DONE"]
   
-  def initialize(id, description, status, created_at, updated_at)
+  def initialize(description)
     @id = self.class.next_id
     @description = description
     @status = "TODO"
@@ -15,8 +15,16 @@ class Task
     @updated_at = Time.now
   end
 
+  def add(description)
+    added_task = Task.new(description)
+    # store to JSON
+  end
+
+  private
+
   def self.next_id
     @@last_id += 1
   end
+
   
 end
